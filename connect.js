9e7899fcdfeaa1766cb344a29f33855bad6b67c1
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-08-12 14:18:04 -0500

    html update

---
+++ connect.js
@@ -0,0 +1,26 @@
+console.log("hello connect four");
+/*--------------- constants --------------*/
+
+
+
+
+/*-------------- game's state (variables) -----------------*/
+
+
+
+
+
+/*------------ cached/store element references ----------------*/
+
+
+
+
+
+/*--------------- event listeners -------------*/
+
+
+
+
+
+
+/*-------------- functions ---------------*/
