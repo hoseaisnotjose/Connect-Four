8e95a6b6b5747f7328ff968bfab22ab79a87df9b
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-08-13 02:15:01 -0500

    creating table using js

--- connect.js
+++ connect.js
@@ -2,54 +2,79 @@ console.log("hello connect four");
 /*--------------- constants --------------*/
 const lookUp = {
     '1': 'red',
-    // '-1': 'green',
+    '-1': 'green',
     'null': 'black'
 }
 
-
+const gameBoard = [
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null]
+];
 
 
 /*-------------- game's state (variables) -----------------*/
 let circle,
-let wrongTurn,
-let turn,
-let winner,
+wrongTurn,
+turn,
+winner
 
 
 
 
 /*------------ cached/store element references ----------------*/
-document.querySelectorAll('.td')
+document.querySelectorAll('td')
 document.querySelectorAll('tr')
 document.querySelectorAll('table')
 
 
 
 /*--------------- event listeners -------------*/
-document.querySelector('table').addEventListener('click', handleMove);
-document.querySelector('#n').addEventListener('click', initialize);
+document.querySelector('#n').addEventListener('click', init);
 
 
 
 /*-------------- functions ---------------*/
-init();
+const render = () => {
+    const table = document.createElement('table');
+    table.addEventListener('click', handleMove)
+    for (let i = 0; i < gameBoard.length; i++) {
+        const tr = document.createElement('tr')
+        tr.id = `row${i}`
+        for (let y = 0; y < gameBoard[i].length; y++) {
+            const td = document.createElement('td')
+            td.className = `c${y} col${y}`
+            gameBoard[i][y]
+                ? td.className+= ` p${gameBoard[i][y]}`
+                : null
+            tr.appendChild(td)
+        }   
+        table.appendChild(tr)
+    }
+    document.getElementById("gameBoard").appendChild(table)
+}
 
-function initialize() {
-    gameBoard = [
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null]
-    ];
+function handleMove(e) {
+    console.log(e);
+}
+function init() {
+    
     turn = 1;
     winner = null;
     render();
 }
 
+// function theWinner() {
+//     for (let i = 0; i < winningCombos.length; i++)
+// }
+
 wrongTurn = [];
 
+
+init()
 
     // Finish initializing state before calling render()
 
