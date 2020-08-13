06594642878fe44ab57ca42ee9185ad3379180e7
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-08-12 19:14:36 -0500

    game board setup

--- connect.js
+++ connect.js
@@ -1,11 +1,22 @@
 console.log("hello connect four");
 /*--------------- constants --------------*/
+const gameBoard = [
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+]
 
+const gameWin = {
+    
+}
 
 
 
 /*-------------- game's state (variables) -----------------*/
-
+turn
 
 
 
