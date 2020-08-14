2a19d7d9c6ea6ff1d783d47caaabf42081873f16
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-08-13 21:44:51 -0500

    gameBoard color assignment, correctly rendering, current player declaration

--- connect.js
+++ connect.js
@@ -19,41 +19,71 @@ const gameBoard = [
 /*-------------- game's state (variables) -----------------*/
 let circle,
 wrongTurn,
-turn,
+currentPlayer,
 winner
 
 
 
 
 /*------------ cached/store element references ----------------*/
-const tableData = document.querySelectorAll('td')
-document.querySelectorAll('tr')
-document.querySelectorAll('table')
+const tableData = document.querySelectorAll('td');
+// document.querySelectorAll('tr')
+// document.querySelectorAll('table')
 
 
 
 /*--------------- event listeners -------------*/
 document.querySelector('#n').addEventListener('click', init);
-// document.querySelector('table').addEventListener('click', )
+// document.querySelector('table').addEventListener('click', handleMove)
 
 
 
 /*-------------- functions ---------------*/
 function handleMove(e) {
-    // console.log(e.target.cellIndex)
-  // get row and column number from e.target (refer to console/inspect)
-  // next, use row/column number to access gameBoard[row number][circle number]
-  // change position in gameBoard array to player  
-    checkCol(e.target.cellIndex)
+//     // console.log(e.target.cellIndex)
+//   // get row and column number from e.target (refer to console/inspect)
+//   // next, use row/column number to access gameBoard[row number][circle number]
+//   // change position in gameBoard array to player  
+    clickSlots(e.target.cellIndex)
+}
 
+for (let i = 0; i < tableData.length; i++) {
+    tableData[i].addEventListener('click', (e) => {
+        clickSlots(e.target.cellIndex);
+    })
+}
 
+function clickSlots(colIdx) {
+    console.log(colIdx);
+    if (gameBoard[0][colIdx]) {
+        console.log('this is filled');
+        return;
     }
+    let isFilled = true;
+    let row = 5;
+    while(isFilled) {
+        if (gameBoard[row][colIdx] == null) {
+            gameBoard[row][colIdx] = currentPlayer;
+            isFilled = false;
+            currentPlayer *= -1;
+            console.log(currentPlayer);
+        } else {
+            // check the next row
+            row--;
+        }
+        
+    }
+    console.log(gameBoard);
+    // check "win call" here
+    render();
+}
     
 // function checkCol(colIndex) {
     // start at the bottom of the column. If that's occupied, we'll move up and check
   
 
 const render = () => {
+    document.getElementById("gameBoard").innerHTML = ''
     const table = document.createElement('table');
     table.addEventListener('click', handleMove)
     for (let i = 0; i < gameBoard.length; i++) {
@@ -77,9 +107,10 @@ const render = () => {
     document.getElementById("gameBoard").appendChild(table)
 }
 
-
+init();
 function init() {
     
+    currentPlayer = 1;
     turn = 1;
     winner = null;
     render();
@@ -92,6 +123,6 @@ function init() {
 wrongTurn = [];
 
 
-// init()
+init()
 
 // Finish initializing state before calling render()
